6f06ecdfd94db1c76d5ffa945a9b8d398f6109fb
Andrew Hallmark <Andrew@TheUniverse>
2022-06-21 16:48:22 +0100

    Global usings and project output type change to get the client library building

---
+++ Cherwell.Api/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using Cherwell.Api.Model.Approval;
+global using Cherwell.Model.BusinessObject;
+global using Cherwell.Model.Core;
+global using Cherwell.Model.Forms;
+global using Cherwell.Model.Lifecycle;
+global using Cherwell.Model.OneStepActions;
+global using Cherwell.Model.Queues;
+global using Cherwell.Model.Searches;
