47b2cdc74307f012a5d0eb657ce145303c1ab23f
Lorna <68371985+geckogeckogecko@users.noreply.github.com>
2022-06-21 08:47:01 +0100

    Added Swagger-gen code

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IO.Swagger.Api;
+using IO.Swagger.Client;
+
+namespace Cherwell.Api
+{
+	public class Program
+	{
+		static async void Main(string[] args)
+		{
+			HttpClient _client = new HttpClient();
+			var apiClient = new ApiClient(_client);
+
+			//var response = await apiClient.SendAsync
+		}
+	}
+}
