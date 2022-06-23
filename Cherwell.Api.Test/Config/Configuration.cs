5db2dfee1f7f31cdd2d8a3409bc402cbc32d871d
Lorna Harvey <lorna_harvey@hotmail.com>
2022-06-23 21:02:13 +0100

    Merged

---
+++ Cherwell.Api.Test/Config/Configuration.cs
@@ -0,0 +1,13 @@
+﻿namespace Cherwell.Api.Test.Config;
+internal class Configuration
+{
+   public CherwellClientOptions? CherwellClientOptions { get; set; }
+
+   internal void Validate()
+   {
+	  if (CherwellClientOptions is null)
+	  {
+		 throw new ConfigurationException($"Missing {nameof(CherwellClientOptions)} in config.");
+	  }
+   }
+}
