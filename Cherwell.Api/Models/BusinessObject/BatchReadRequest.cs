01b61541ea3b9714d94162ef9209241e3d7cc33b
David Bond <david@davidbond.net>
2022-06-24 11:47:48 +0100

    Tidied ReadRequest and BatchReadRequest

--- Cherwell.Api/Models/BusinessObject/BatchReadRequest.cs
+++ Cherwell.Api/Models/BusinessObject/BatchReadRequest.cs
@@ -1,7 +1,4 @@
-using Newtonsoft.Json;
-using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
-using System.Text;
 
 namespace Cherwell.Api.Models.BusinessObject;
 
@@ -9,123 +6,17 @@ namespace Cherwell.Api.Models.BusinessObject;
 /// BusinessObjectBatchReadRequest
 /// </summary>
 [DataContract]
-public partial class BatchReadRequest : IEquatable<BatchReadRequest>, IValidatableObject
+public partial class BatchReadRequest
 {
-	/// <summary>
-	/// Initializes a new instance of the <see cref="BatchReadRequest" /> class.
-	/// </summary>
-	/// <param name="ReadRequests">ReadRequests.</param>
-	/// <param name="StopOnError">StopOnError.</param>
-	public BatchReadRequest(List<ReadRequest> ReadRequests, bool? StopOnError = default)
-	{
-		this.ReadRequests = ReadRequests;
-		this.StopOnError = StopOnError;
-	}
-
 	/// <summary>
 	/// Gets or Sets ReadRequests
 	/// </summary>
 	[DataMember(Name = "readRequests", EmitDefaultValue = false)]
-	public List<ReadRequest> ReadRequests { get; set; }
+	public List<ReadRequest>? ReadRequests { get; set; }
 
 	/// <summary>
 	/// Gets or Sets StopOnError
 	/// </summary>
 	[DataMember(Name = "stopOnError", EmitDefaultValue = false)]
 	public bool? StopOnError { get; set; }
-
-	/// <summary>
-	/// Returns the string presentation of the object
-	/// </summary>
-	/// <returns>String presentation of the object</returns>
-	public override string ToString()
-	{
-		var sb = new StringBuilder();
-		sb.Append("class BusinessObjectBatchReadRequest {\n");
-		sb.Append("  ReadRequests: ").Append(ReadRequests).Append('\n');
-		sb.Append("  StopOnError: ").Append(StopOnError).Append('\n');
-		sb.Append("}\n");
-		return sb.ToString();
-	}
-
-	/// <summary>
-	/// Returns the JSON string presentation of the object
-	/// </summary>
-	/// <returns>JSON string presentation of the object</returns>
-	public string ToJson()
-	{
-		return JsonConvert.SerializeObject(this, Formatting.Indented);
-	}
-
-	/// <summary>
-	/// Returns true if objects are equal
-	/// </summary>
-	/// <param name="obj">Object to be compared</param>
-	/// <returns>Boolean</returns>
-	public override bool Equals(object? obj)
-	{
-		// credit: http://stackoverflow.com/a/10454552/677735
-		return Equals(obj as BatchReadRequest);
-	}
-
-	/// <summary>
-	/// Returns true if BusinessObjectBatchReadRequest instances are equal
-	/// </summary>
-	/// <param name="other">Instance of BusinessObjectBatchReadRequest to be compared</param>
-	/// <returns>Boolean</returns>
-	public bool Equals(BatchReadRequest? other)
-	{
-		// credit: http://stackoverflow.com/a/10454552/677735
-		if (other == null)
-		{
-			return false;
-		}
-
-		return
-				(
-					ReadRequests == other.ReadRequests ||
-					ReadRequests != null &&
-					ReadRequests.SequenceEqual(other.ReadRequests)
-				) &&
-				(
-					StopOnError == other.StopOnError ||
-					StopOnError != null &&
-					StopOnError.Equals(other.StopOnError)
-				);
-	}
-
-	/// <summary>
-	/// Gets the hash code
-	/// </summary>
-	/// <returns>Hash code</returns>
-	public override int GetHashCode()
-	{
-		// credit: http://stackoverflow.com/a/263416/677735
-		unchecked // Overflow is fine, just wrap
-		{
-			int hash = 41;
-			// Suitable nullity checks etc, of course :)
-			if (ReadRequests != null)
-			{
-				hash = hash * 59 + ReadRequests.GetHashCode();
-			}
-
-			if (StopOnError != null)
-			{
-				hash = hash * 59 + StopOnError.GetHashCode();
-			}
-
-			return hash;
-		}
-	}
-
-	/// <summary>
-	/// To validate all properties of the instance
-	/// </summary>
-	/// <param name="validationContext">Validation context</param>
-	/// <returns>Validation Result</returns>
-	IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-	{
-		yield break;
-	}
 }
--- Cherwell.Api/Models/BusinessObject/ReadRequest.cs
+++ Cherwell.Api/Models/BusinessObject/ReadRequest.cs
@@ -1,7 +1,4 @@
-using Newtonsoft.Json;
-using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
-using System.Text;
 
 namespace Cherwell.Api.Models.BusinessObject;
 
@@ -9,142 +6,23 @@ namespace Cherwell.Api.Models.BusinessObject;
 /// BusinessObjectReadRequest
 /// </summary>
 [DataContract]
-public partial class ReadRequest : IEquatable<ReadRequest>, IValidatableObject
+public partial class ReadRequest
 {
-	/// <summary>
-	/// Initializes a new instance of the <see cref="ReadRequest" /> class.
-	/// </summary>
-	/// <param name="BusObId">BusObId.</param>
-	/// <param name="BusObPublicId">BusObPublicId.</param>
-	/// <param name="BusObRecId">BusObRecId.</param>
-	public ReadRequest(string BusObId, string BusObPublicId, string BusObRecId)
-	{
-		this.BusObId = BusObId;
-		this.BusObPublicId = BusObPublicId;
-		this.BusObRecId = BusObRecId;
-	}
-
 	/// <summary>
 	/// Gets or Sets BusObId
 	/// </summary>
 	[DataMember(Name = "busObId", EmitDefaultValue = false)]
-	public string BusObId { get; set; }
+	public string? BusObId { get; set; }
 
 	/// <summary>
 	/// Gets or Sets BusObPublicId
 	/// </summary>
 	[DataMember(Name = "busObPublicId", EmitDefaultValue = false)]
-	public string BusObPublicId { get; set; }
+	public string? BusObPublicId { get; set; }
 
 	/// <summary>
 	/// Gets or Sets BusObRecId
 	/// </summary>
 	[DataMember(Name = "busObRecId", EmitDefaultValue = false)]
-	public string BusObRecId { get; set; }
-
-	/// <summary>
-	/// Returns the string presentation of the object
-	/// </summary>
-	/// <returns>String presentation of the object</returns>
-	public override string ToString()
-	{
-		var sb = new StringBuilder();
-		sb.Append("class BusinessObjectReadRequest {\n");
-		sb.Append("  BusObId: ").Append(BusObId).Append('\n');
-		sb.Append("  BusObPublicId: ").Append(BusObPublicId).Append('\n');
-		sb.Append("  BusObRecId: ").Append(BusObRecId).Append('\n');
-		sb.Append("}\n");
-		return sb.ToString();
-	}
-
-	/// <summary>
-	/// Returns the JSON string presentation of the object
-	/// </summary>
-	/// <returns>JSON string presentation of the object</returns>
-	public string ToJson()
-	{
-		return JsonConvert.SerializeObject(this, Formatting.Indented);
-	}
-
-	/// <summary>
-	/// Returns true if objects are equal
-	/// </summary>
-	/// <param name="obj">Object to be compared</param>
-	/// <returns>Boolean</returns>
-	public override bool Equals(object? obj)
-	{
-		// credit: http://stackoverflow.com/a/10454552/677735
-		return Equals(obj as ReadRequest);
-	}
-
-	/// <summary>
-	/// Returns true if BusinessObjectReadRequest instances are equal
-	/// </summary>
-	/// <param name="other">Instance of BusinessObjectReadRequest to be compared</param>
-	/// <returns>Boolean</returns>
-	public bool Equals(ReadRequest? other)
-	{
-		// credit: http://stackoverflow.com/a/10454552/677735
-		if (other == null)
-		{
-			return false;
-		}
-
-		return
-				(
-					BusObId == other.BusObId ||
-					BusObId != null &&
-					BusObId.Equals(other.BusObId, StringComparison.Ordinal)
-				) &&
-				(
-					BusObPublicId == other.BusObPublicId ||
-					BusObPublicId != null &&
-					BusObPublicId.Equals(other.BusObPublicId, StringComparison.Ordinal)
-				) &&
-				(
-					BusObRecId == other.BusObRecId ||
-					BusObRecId != null &&
-					BusObRecId.Equals(other.BusObRecId, StringComparison.Ordinal)
-				);
-	}
-
-	/// <summary>
-	/// Gets the hash code
-	/// </summary>
-	/// <returns>Hash code</returns>
-	public override int GetHashCode()
-	{
-		// credit: http://stackoverflow.com/a/263416/677735
-		unchecked // Overflow is fine, just wrap
-		{
-			int hash = 41;
-			// Suitable nullity checks etc, of course :)
-			if (BusObId != null)
-			{
-				hash = hash * 59 + BusObId.GetHashCode();
-			}
-
-			if (BusObPublicId != null)
-			{
-				hash = hash * 59 + BusObPublicId.GetHashCode();
-			}
-
-			if (BusObRecId != null)
-			{
-				hash = hash * 59 + BusObRecId.GetHashCode();
-			}
-
-			return hash;
-		}
-	}
-
-	/// <summary>
-	/// To validate all properties of the instance
-	/// </summary>
-	/// <param name="validationContext">Validation context</param>
-	/// <returns>Validation Result</returns>
-	IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-	{
-		yield break;
-	}
+	public string? BusObRecId { get; set; }
 }
