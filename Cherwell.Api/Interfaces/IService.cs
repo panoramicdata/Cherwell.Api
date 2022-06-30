671cc9111157bea102363cb92beaf7c8c3ece83c
andrewhallmark <andrew.hallmark@panoramic.systems>
2022-06-30 09:05:00 +0100

    Removed security-related methods; these are dealt with automatically and need not be exposed publicly

--- Cherwell.Api/Interfaces/IService.cs
+++ Cherwell.Api/Interfaces/IService.cs
@@ -18,44 +18,4 @@ public interface IService
 	[Get("/api/V1/serviceinfo")]
 	Task<ServiceInfoResponse> GetServiceInfoAsync(
 		CancellationToken cancellationToken);
-
-	/// <summary>
-	/// Log out user by token
-	/// </summary>
-	/// <remarks>
-	/// Operation that logs out the user referenced in the authentication token.
-	/// </remarks>
-	/// <exception cref="Client.ApiException">Thrown when fails to make API call</exception>
-	/// <returns>Task of void</returns>
-	[Delete("/api/V1/logout")]
-	Task LogoutUserAsync(
-		CancellationToken cancellationToken);
-
-	/// <summary>
-	/// Get an access token
-	/// </summary>
-	/// <remarks>
-	/// Operation to request an access token for one of the following authentication modes. Or, you can request an access token using a refresh token. An API client key is required in both cases, and the authentication mode you use must be the mode used by the CSM Browser Client. &lt;/br&gt; Internal - Use a CSM username and password. If no other mode is specified, Internal mode is used.&lt;/br&gt; Windows - Uses the server variable LOGON_USER to attempt to find a CSM user.  You can also use domain\\username and password.&lt;/br&gt; LDAP - Uses the LDAP settings configured for CSM and the server variable LOGON_USER to attempt to find a CSM user. You can also use domain\\username and password.&lt;/br&gt; SAML - Uses the SAML settings configured for CSM to validate credentials and find the CSM user.&lt;/br&gt; Auto - Uses the enabled authentication types configured for CSM.
-	/// </remarks>
-	/// <exception cref="Client.ApiException">Thrown when fails to make API call</exception>
-	/// <param name="grant_type">The type of grant being requested: password or refresh_token.</param>
-	/// <param name="client_id">The API client key for the client making the token request.</param>
-	/// <param name="client_secret">The API client secret for the native client making the token request.  This is only required for native clients. (optional)</param>
-	/// <param name="username">Specify the login ID for the CSM user who will be granted the access token.  (optional)</param>
-	/// <param name="password">Specify the password assigned to the login ID. (optional)</param>
-	/// <param name="refresh_token">Specify the refresh token used to grant another access token. (optional)</param>
-	/// <param name="auth_mode">Specify the Authentication Mode to use for requesting an access token. (optional)</param>
-	/// <param name="site_name">If for portal specify the Site name to use for requesting an access token. (optional)</param>
-	/// <returns>Task of TokenResponse</returns>
-	[Post("/token")]
-	Task<TokenResponse> TokenAsync(
-		string grantType,
-		string clientId,
-		string clientSecret,
-		string username,
-		string password,
-		string refreshToken,
-		[AliasAs("auth_mode")] string authMode,
-		[AliasAs("site_name")] string? siteName,
-		CancellationToken cancellationToken);
 }
