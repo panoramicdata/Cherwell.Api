94aa19e358682bf79cf92fc3d188cbcb75d3a6c9
andrewhallmark <46025270+TheCakeMonster@users.noreply.github.com>
2024-04-29 17:26:30 +0100

    Added ServiceTests for basic connectivity checks

---
+++ Cherwell.Api.Test/ServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Cherwell.Api.Test;
+
+public class ServiceTests(ITestOutputHelper iTestOutputHelper) : CherwellClientTest(iTestOutputHelper)
+{
+	[Fact]
+	public async Task GetServiceInfoAsync_RequestServiceInfo_GetsNotNullResponse()
+	{
+		var response = await TestCherwellClient
+			.Service
+			.GetServiceInfoAsync(CancellationToken.None);
+
+		response
+			.Should()
+			.NotBeNull();
+	}
+
+	[Fact]
+	public async Task GetServiceInfoAsync_RequestServiceInfo_GetsValidApiVersion()
+	{
+		var response = await TestCherwellClient
+			.Service
+			.GetServiceInfoAsync(CancellationToken.None);
+
+		response
+			.ApiVersion
+			.Should()
+			.NotBeNullOrWhiteSpace();
+	}
+
+	[Fact]
+	public async Task GetServiceInfoAsync_RequestServiceInfo_GetsSystemDateThatIsToday()
+	{
+		var response = await TestCherwellClient
+			.Service
+			.GetServiceInfoAsync(CancellationToken.None);
+
+		response
+			.SystemDateTime
+			.Date
+			.Should()
+			.Be(DateTime.Today);
+	}
+}
